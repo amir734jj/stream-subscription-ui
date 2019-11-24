27f39b5bf43844a6b2889bc13e7a994542a3a823
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2019-11-24 11:55:49 -0600

    More progress

--- app/src/app/services/authentication.service.ts
+++ app/src/app/services/authentication.service.ts
@@ -1,33 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import {map, tap} from 'rxjs/operators';
-import {LoginResponse} from '../models/authentication.service/login/LoginResponse';
 import {LoginRequest} from '../models/authentication.service/login/LoginRequest';
 import {RegisterRequest} from '../models/authentication.service/register/RegisterRequest';
 import {RegisterResponse} from '../models/authentication.service/register/RegisterResponse';
 import route from '../utilities/route.utility';
 import {Role} from '../models/RoleEnum';
+// @ts-ignore
+import jwtDecode from 'jwt-decode';
 
 @Injectable()
 export class AuthenticationService {
-  public token: string;
-
   constructor(private http: HttpClient) { }
 
   login(loginRequest: LoginRequest) {
-    return this.http.post<LoginResponse>(route('account', 'login'), loginRequest)
-      .pipe(
-        map((response: LoginResponse) => {
-          // login successful if there's a jwt token in the response
-          this.token = response.token;
-          const email = response.email;
-          if (this.token) {
-            // store email and jwt token in local storage to keep user logged in between page refreshes
-            localStorage.setItem('user', JSON.stringify({ email, token: this.token }));
-          }
-          return response;
-        })
-      );
+    return this.http.post(route('account', 'login'), loginRequest, { responseType: 'text' })
+      .pipe(tap(token => {
+        // login successful if there's a jwt token in the response
+        if (token) {
+          const metadata = jwtDecode(token);
+          // store email and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('user', JSON.stringify({ ...metadata, token }));
+        }
+      }));
   }
 
   register(role: Role, registerRequest: RegisterRequest) {
@@ -43,7 +38,6 @@ export class AuthenticationService {
     return this.http.post(route('account', 'logout'), { responseType: 'text' })
       .pipe(tap(_ => {
         // clear token remove user from local storage to log user out
-        this.token = null;
         localStorage.removeItem('user');
       }));
   }
