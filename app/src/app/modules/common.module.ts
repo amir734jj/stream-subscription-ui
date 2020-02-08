8211b9f9195255b4a2f6d2ee374e118571a88957
Amir <hesamian@uwm.edu>
2020-02-08 11:29:16 -0600

    Working progress bar

--- app/src/app/modules/common.module.ts
+++ app/src/app/modules/common.module.ts
@@ -5,12 +5,22 @@ import {RequestInterceptor} from '../utilities/custom.error.handler.utility';
 import {HttpInterceptComponent} from '../components/common/http-intercept/http-intercept.component';
 import {CommonComponent} from '../components/common/wrapper/common.component';
 import {CommonModule} from '@angular/common';
+// for HttpClient import:
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
+
+// for Router import:
+import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     CommonModule,
     AlertModule.forRoot(),
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    // for HttpClient use:
+    LoadingBarHttpClientModule,
+    // for Router use:
+    LoadingBarRouterModule,
   ],
   providers: [BsModalService, RequestInterceptor, {
     provide: HTTP_INTERCEPTORS,
