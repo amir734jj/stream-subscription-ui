f0ca943105fbb409d935f1edbb15bd6527b377f8
Amir <hesamian@uwm.edu>
2020-05-09 02:06:27 -0500

    bug fixed

--- app/src/app/models/constants/routeStore.ts
+++ app/src/app/models/constants/routeStore.ts
@@ -1,4 +1,4 @@
 import {DetachedRouteHandle} from '@angular/router';
 import { Map } from 'immutable';
 
-export const routeStore = Map<string, DetachedRouteHandle>();
+export const routeStore = Map<string, DetachedRouteHandle>().asMutable();
