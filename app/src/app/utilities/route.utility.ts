60a001f94878c7b403a004ff1e36ccd9ad5ae90b
Amir <hesamian@uwm.edu>
2020-05-09 09:56:26 -0500

    more UI improvement

--- app/src/app/utilities/route.utility.ts
+++ app/src/app/utilities/route.utility.ts
@@ -2,6 +2,6 @@ import urlJoin from 'url-join';
 import {environment} from '../../environments/environment';
 
 const apiUrl = environment.apiUrl;
-const route = (...arg: any[]) => urlJoin(apiUrl, ...arg.map(x => x.toString()));
+const route = (...arg: any[]) => urlJoin(apiUrl, ...arg.filter(x => x).map(x => x.toString()));
 
 export default route;
