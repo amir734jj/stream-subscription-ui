b317c4295cb1fa82dff21acb083c42bb57537765
Seyedamirhossein Hesamian <seyedhesamian@northwesternmutual.com>
2020-05-13 12:49:13 -0500

    more cleanup

--- app/src/app/utilities/injectables/mediaSession.utility.ts
+++ app/src/app/utilities/injectables/mediaSession.utility.ts
@@ -4,19 +4,21 @@ import {
   MediaPlaybackHandlersT,
   MediaSessionAction,
   MediaSessionPlaybackState,
+  MediaSession,
 } from '../../types/mediaSession.type';
 
 @Injectable()
 export class MediaSessionUtility {
+
   // @ts-ignore
-  isMediaSessionAvailable = () => {
-    return window && window.navigator && 'mediaSession' in window.navigator;
-  }
+  private mediaSession = navigator.mediaSession as MediaSession
+
+  private mediaSessionAvailable = window && window.navigator && 'mediaSession' in window.navigator
 
   setMetadata = (songMetadata: { artist: string, title: string }) => {
-    if (this.isMediaSessionAvailable()) {
+    if (this.mediaSessionAvailable) {
       // @ts-ignore
-      navigator.mediaSession.metadata = new MediaMetadata({
+      this.mediaSession.metadata = new MediaMetadata({
         title: songMetadata.title,
         artist: songMetadata.artist,
         album: 'unknown',
@@ -35,24 +37,17 @@ export class MediaSessionUtility {
       onSeekForward: _.noop
     }, options);
 
-    if (this.isMediaSessionAvailable()) {
-      // @ts-ignore
-      navigator.mediaSession.setActionHandler(MediaSessionAction.Play, onPlay);
-      // @ts-ignore
-      navigator.mediaSession.setActionHandler(MediaSessionAction.Pause, onPause);
-      // @ts-ignore
-      navigator.mediaSession.setActionHandler(MediaSessionAction.SeekBackward, onSeekBackward);
-      // @ts-ignore
-      navigator.mediaSession.setActionHandler(MediaSessionAction.SeekForward, onSeekForward);
-      // @ts-ignore
-      navigator.mediaSession.setActionHandler(MediaSessionAction.PreviousTrack, onPreviousTrack);
-      // @ts-ignore
-      navigator.mediaSession.setActionHandler(MediaSessionAction.NextTrack, onNextTrack);
+    if (this.mediaSessionAvailable) {
+      this.mediaSession.setActionHandler(MediaSessionAction.Play, onPlay);
+      this.mediaSession.setActionHandler(MediaSessionAction.Pause, onPause);
+      this.mediaSession.setActionHandler(MediaSessionAction.SeekBackward, onSeekBackward);
+      this.mediaSession.setActionHandler(MediaSessionAction.SeekForward, onSeekForward);
+      this.mediaSession.setActionHandler(MediaSessionAction.PreviousTrack, onPreviousTrack);
+      this.mediaSession.setActionHandler(MediaSessionAction.NextTrack, onNextTrack);
     }
   }
 
   setPlaybackState = (state: MediaSessionPlaybackState) => {
-    // @ts-ignore
-    return navigator.mediaSession.playbackState = state;
+    return this.mediaSession.playbackState = state;
   }
 }
